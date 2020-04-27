ea314b4621b09c89170884e9095c4c5112ad5341
unknown <levi.ebora@gmail.com>
2020-04-27 09:37:20 +0800

    finalized docker implem

---
+++ app/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request, jsonify
+from fastai import *
+from fastai.vision import *
+from io import BytesIO
+
+classes = ['Abyssinian','Bengal','Birman','Bombay','British_Shorthair','Egyptian_Mau','Maine_Coon','Persian','Ragdoll','Russian_Blue','Siamese','Sphynx','american_bulldog','american_pit_bull_terrier','basset_hound','beagle','boxer','chihuahua','english_cocker_spaniel','english_setter','german_shorthaired','great_pyrenees','havanese','japanese_chin','keeshond','leonberger','miniature_pinscher','newfoundland','pomeranian','pug','saint_bernard','samoyed','scottish_terrier','shiba_inu','staffordshire_bull_terrier','wheaten_terrier','yorkshire_terrier']
+
+learn = load_learner(Path('app/models'), 'resnet50_revalida.pkl')
+
+app = Flask(__name__)
+
+@app.route('/', methods=["GET", "POST"])
+def index():
+  if(request.method == "POST"):
+    pass
+  else:
+    return render_template("index.html")
+
+@app.route('/classify', methods=['POST'])
+def classify():
+  image = ""
+  if request.files:
+    image = request.files["inputFile"]
+    
+  image = open_image(BytesIO(image.read()))
+  prediction = learn.predict(image)[0]
+  return jsonify(result=str(prediction))
+
+if __name__ == '__main__':
+  app.run(host="0.0.0.0", port = 5000, debug = True)
---
+++ app/static/app.js
@@ -0,0 +1,35 @@
+const uploadImageForm = document.getElementById("upload-image-form");
+const inputFile = document.getElementById("input-file");
+const uploadedImage = document.getElementById("uploaded-image");
+const resultText = document.getElementById("result-text");
+let imgURL = ""
+
+uploadImageForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  let reader = new FileReader();
+  reader.onloadend = function () {
+    uploadedImage.src = reader.result
+  }
+
+  reader.readAsDataURL(inputFile.files[0]);
+
+  formData = new FormData()
+  formData.append("inputFile", inputFile.files[0])
+
+  let loc = window.location
+
+  fetch(`${loc.protocol}//${loc.hostname}:${loc.port}/classify`, {
+    method: 'POST',
+    body: formData
+  }).then(response => {
+    return response.json()
+  }).then(text => {
+    console.log(text)
+    resultText.innerHTML = `This image is a ${text.result}!`
+  })
+
+});
+
+
+
