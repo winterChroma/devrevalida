bcbbe14d564501d974e3b2b4b7ac83b2d6e16b78
unknown <levi.ebora@gmail.com>
2020-04-27 10:54:06 +0800

    download models upon running the container

--- app/app.py
+++ app/app.py
@@ -5,6 +5,7 @@ from io import BytesIO
 
 classes = ['Abyssinian','Bengal','Birman','Bombay','British_Shorthair','Egyptian_Mau','Maine_Coon','Persian','Ragdoll','Russian_Blue','Siamese','Sphynx','american_bulldog','american_pit_bull_terrier','basset_hound','beagle','boxer','chihuahua','english_cocker_spaniel','english_setter','german_shorthaired','great_pyrenees','havanese','japanese_chin','keeshond','leonberger','miniature_pinscher','newfoundland','pomeranian','pug','saint_bernard','samoyed','scottish_terrier','shiba_inu','staffordshire_bull_terrier','wheaten_terrier','yorkshire_terrier']
 
+model_path = download_url('https://revalida-test.s3-ap-southeast-1.amazonaws.com/resnet50_revalida.pkl','app/models/resnet50_revalida.pkl')
 learn = load_learner(Path('app/models'), 'resnet50_revalida.pkl')
 
 app = Flask(__name__)
@@ -21,10 +22,11 @@ def classify():
   image = ""
   if request.files:
     image = request.files["inputFile"]
-    
+
   image = open_image(BytesIO(image.read()))
   prediction = learn.predict(image)[0]
-  return jsonify(result=str(prediction))
+  print(prediction)
+  return {'result': str(prediction)}
 
 if __name__ == '__main__':
   app.run(host="0.0.0.0", port = 5000, debug = True)
